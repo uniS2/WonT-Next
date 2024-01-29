f6d911bfc222605ed30cca6543d8e627d7e35f28
uniS2 <s2oie1010@gmail.com>
2024-01-29 20:41:08 +0900

    Fix: window `"endOfLine" 버그 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
-import '@/styles/globals.css';
-import type { Metadata } from 'next';
-import Header from '@/components/header/Header';
-import Footer from '@/components/footer/Footer';
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
 
 export const metadata: Metadata = {
-  title: 'WonT',
-  description: '행복한 여행을 위한 여행일정 플랫폼입니다.',
+  title: "WonT",
+  description: "행복한 여행을 위한 여행일정 플랫폼입니다.",
   icons: {
-    icon: '/favicon/favicon.ico',
+    icon: "/favicon/favicon.ico",
   },
-  keywords: ['여행', '한국', '국내', 'korea', 'trip', 'plan'],
+  keywords: ["여행", "한국", "국내", "korea", "trip", "plan"],
 };
 
 export default function RootLayout({
