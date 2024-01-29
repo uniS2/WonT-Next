a7b9050287b1414446431b498cf74b9be6c00922
Jeon SunYong <sunyong0725@naver.com>
2024-01-29 18:34:29 +0900

    Test : Test 코드 첨부합니다. 테스트하고 모두 다 삭제하고 다시 푸시할 예정입니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
-import "@/styles/globals.css";
-import type { Metadata } from "next";
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
+import '@/styles/globals.css';
+import type { Metadata } from 'next';
+import Header from '@/components/header/Header';
+import Footer from '@/components/footer/Footer';
 
 export const metadata: Metadata = {
-  title: "WonT",
-  description: "행복한 여행을 위한 여행일정 플랫폼입니다.",
+  title: 'WonT',
+  description: '행복한 여행을 위한 여행일정 플랫폼입니다.',
   icons: {
-    icon: "/favicon/favicon.ico",
+    icon: '/favicon/favicon.ico',
   },
-  keywords: ["여행", "한국", "국내", "korea", "trip", "plan"],
+  keywords: ['여행', '한국', '국내', 'korea', 'trip', 'plan'],
 };
 
 export default function RootLayout({
@@ -18,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko-KR">
+    <html lang="ko-KR" suppressHydrationWarning={true}>
       <body>
         <Header />
-        {children}
+        <main className="flex h-auto w-auto flex-col items-center justify-center">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
   return (
-    <div className="bg-point">
+    <div className="flex h-[10rem] w-auto min-w-[360px] flex-col items-center justify-center bg-point">
       <h1>Footer입니다 Test중입니다.</h1>
     </div>
   );
