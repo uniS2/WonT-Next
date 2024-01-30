2b8a24534f45ad32af0a8f1b53406067b9a33ca7
Jeon SunYong <sunyong0725@naver.com>
2024-01-30 21:14:20 +0900

    Style : 크기 수정 크기 수정

--- src/components/sign/SignInput.tsx
+++ src/components/sign/SignInput.tsx
@@ -8,7 +8,7 @@ interface SignInputProps {
 
 const SignInput: React.FC<SignInputProps> = ({ text, placeholder, type }) => {
   return (
-    <div className="pt-[3rem] min-w-[18.75rem] sm:min-w-[31.25rem] lg:min-w-[43.75rem] 2xl:min-w-[56.25rem] flex flex-col gap-3 border-b-2 border-primary mx-auto pb-[0.313rem]">
+    <div className="pt-[1.5rem] min-w-[18.75rem] sm:min-w-[31.25rem] lg:min-w-[43.75rem] 2xl:min-w-[56.25rem] flex flex-col gap-3 border-b-2 border-primary mx-auto pb-[0.313rem]">
       <p className="text-[1rem] sm:text-[1.25rem] lg:text-[1.563rem]">{text}</p>
       <input
         type={type}
