f89c67d3e044bad7cc29435317a6dbe9e9c81a27
Jeon SunYong <sunyong0725@naver.com>
2024-01-30 14:17:00 +0900

    Feat : src/pages/index.tsx 생성

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,14 @@
+import SignInLayout from "@/app/signin/layout";
+
+function SignInPage() {
+  return (
+    <SignInLayout>
+      <div>
+        <h1>왜 안나오는걸까요 ?</h1>
+        <p>왜 갑자기 된거지 ?\"Attemptedimporterror:'$1'isnotexportedfrom</p>
+      </div>
+    </SignInLayout>
+  );
+}
+
+export default SignInPage;
