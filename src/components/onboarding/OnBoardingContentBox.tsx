d453f70c47c998a622f153b76ed371dd41bd633c
Jeon SunYong <sunyong0725@naver.com>
2024-01-31 23:45:30 +0900

    Style : OnBoardingContentBox 1차 컴포넌트 설정

---
+++ src/components/onboarding/OnBoardingContentBox.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function OnBoardingContentBox() {
+  return (
+    <div className="w-[230px] h-[220px] bg-secondary rounded-xl ml-[40px] flex">
+      <video src=""></video>
+    </div>
+  );
+}
+
+export default OnBoardingContentBox;
