da6def27a8920c49f030156461cd49eaa3351b3e
Jeon SunYong <sunyong0725@naver.com>
2024-01-31 23:46:04 +0900

    Style : 레이아웃 설정

---
+++ src/layout/onboarding/layout.tsx
@@ -0,0 +1,26 @@
+import "@/styles/globals.css";
+import Head from "next/head";
+
+function OnBoardingLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Head>
+        <title>Sign In - WonT</title>
+        <meta
+          name="description"
+          content="행복한 여행을 위한 여행일정 플랫폼입니다."
+        />
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </Head>
+      <section className="flex h-auto w-auto flex-col items-center">
+        {children}
+      </section>
+    </>
+  );
+}
+
+export default OnBoardingLayout;
