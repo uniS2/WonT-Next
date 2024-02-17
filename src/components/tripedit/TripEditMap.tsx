5c7d67313c4295ab50c88f4da588aadfeb7acec6
hyeonjuuu <janghyeonjuu@gmail.com>
2024-02-17 20:36:31 +0900

    [Feat] 지도에 클릭 이벤트 발생 시 마커 추가

--- src/components/tripedit/TripEditMap.tsx
+++ src/components/tripedit/TripEditMap.tsx
@@ -22,6 +22,37 @@ function TripEditMap() {
         };
 
         var map = new window.kakao.maps.Map(container, options);
+
+        var marker = new window.kakao.maps.Marker({
+          // 지도 중심좌표에 마커를 생성합니다
+          position: map.getCenter(),
+        });
+        // 지도에 마커를 표시합니다
+        marker.setMap(map);
+
+        // 지도에 클릭 이벤트를 등록합니다
+        // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+        window.kakao.maps.event.addListener(
+          map,
+          "click",
+          function (mouseEvent: { latLng: any }) {
+            // 클릭한 위도, 경도 정보를 가져옵니다
+            var latlng = mouseEvent.latLng;
+
+            // 마커 위치를 클릭한 위치로 옮깁니다
+            marker.setPosition(latlng);
+
+            var message = "클릭한 위치의 위도는 " + latlng.getLat() + " 이고, ";
+            message += "경도는 " + latlng.getLng() + " 입니다";
+
+            console.log(message);
+
+            // var resultDiv = document.getElementById("clickLatlng");
+            // if (resultDiv) {
+            //   resultDiv.innerHTML = message;
+            // }
+          },
+        );
       });
     };
 
