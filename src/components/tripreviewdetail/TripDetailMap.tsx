63599b182f2402aa4841aeeb153d03059d36291f
hyeonjuuu <janghyeonjuu@gmail.com>
2024-02-17 22:28:25 +0900

    [Feat] 클릭 이벤트 통해 해당 좌표 정보 표시하기 진행중

--- src/components/tripreviewdetail/TripDetailMap.tsx
+++ src/components/tripreviewdetail/TripDetailMap.tsx
@@ -6,11 +6,11 @@ declare global {
   }
 }
 
-function TripDetailMap() {
+function TripEditMap() {
   useEffect(() => {
     const kakaoMapScript = document.createElement("script");
     kakaoMapScript.async = false;
-    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=700d399006256f95732f06b19c046ba5&autoload=false`;
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=700d399006256f95732f06b19c046ba5&autoload=false&libraries=services`;
     document.head.appendChild(kakaoMapScript);
 
     const onLoadKakaoAPI = () => {
@@ -23,33 +23,114 @@ function TripDetailMap() {
 
         var map = new window.kakao.maps.Map(container, options);
 
-        var positions = [
-          {
-            title: "카카오",
-            latlng: new window.kakao.maps.LatLng(33.450705, 126.570677),
-          },
-          {
-            title: "생태연못",
-            latlng: new window.kakao.maps.LatLng(33.450936, 126.569477),
-          },
-          {
-            title: "텃밭",
-            latlng: new window.kakao.maps.LatLng(33.450879, 126.56994),
-          },
-          {
-            title: "근린공원",
-            latlng: new window.kakao.maps.LatLng(33.451393, 126.570738),
-          },
-        ];
+        // var marker = new window.kakao.maps.Marker({
+        //   // 지도 중심좌표에 마커를 생성합니다
+        //   position: map.getCenter(),
+        // });
 
-        for (var i = 0; i < positions.length; i++) {
-          // 마커를 생성합니다
-          var marker = new window.kakao.maps.Marker({
-            map: map, // 마커를 표시할 지도
-            position: positions[i].latlng, // 마커를 표시할 위치
-            title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-          });
-        }
+        var marker = new window.kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
+          infowindow = new window.kakao.maps.InfoWindow({ zindex: 1 }); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+
+        // 지도에 마커를 표시합니다
+        marker.setMap(map);
+
+        // 지도에 클릭 이벤트를 등록합니다
+        // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+        window.kakao.maps.event.addListener(
+          map,
+          "click",
+          function (mouseEvent: { latLng: any }) {
+            // 클릭한 위도, 경도 정보를 가져옵니다
+            var latlng = mouseEvent.latLng;
+
+            // 마커 위치를 클릭한 위치로 옮깁니다
+            marker.setPosition(latlng);
+
+            var message = "클릭한 위치의 위도는 " + latlng.getLat() + " 이고, ";
+            message += "경도는 " + latlng.getLng() + " 입니다";
+
+            console.log(message);
+          },
+        );
+
+        // 주소-좌표 변환 객체를 생성합니다
+        var geocoder = new window.kakao.maps.services.Geocoder();
+
+        // 주소로 좌표를 검색합니다
+        geocoder.addressSearch(
+          "제주특별자치도 제주시 첨단로 242",
+          function (result: { x: any; y: any }[], status: any) {
+            // 정상적으로 검색이 완료됐으면
+            if (status === window.kakao.maps.services.Status.OK) {
+              var coords = new window.kakao.maps.LatLng(
+                result[0].y,
+                result[0].x,
+              );
+
+              // 결과값으로 받은 위치를 마커로 표시합니다
+              var marker = new window.kakao.maps.Marker({
+                map: map,
+                position: coords,
+              });
+
+              // 인포윈도우로 장소에 대한 설명을 표시합니다
+              var infowindow = new window.kakao.maps.InfoWindow({
+                content:
+                  '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>',
+              });
+              infowindow.open(map, marker);
+
+              // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+              map.setCenter(coords);
+            }
+          },
+        );
+
+        /* -------------------------------------------------------------------------- */
+
+        // if (navigator.geolocation) {
+        //   // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+        //   navigator.geolocation.getCurrentPosition(function (position) {
+        //     var lat = position.coords.latitude, // 위도
+        //       lon = position.coords.longitude; // 경도
+
+        //     var locPosition = new window.kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+        //       message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+
+        //     // 마커와 인포윈도우를 표시합니다
+        //     displayMarker(locPosition, message);
+        //   });
+        // } else {
+        //   // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+        //   var locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667),
+        //     message = "geolocation을 사용할수 없어요..";
+
+        //   displayMarker(locPosition, message);
+        // }
+
+        // function displayMarker(locPosition: any, message: string) {
+        //   // 마커를 생성합니다
+        //   var marker = new window.kakao.maps.Marker({
+        //     map: map,
+        //     position: locPosition,
+        //   });
+
+        //   var iwContent = message, // 인포윈도우에 표시할 내용
+        //     iwRemoveable = true;
+
+        //   // 인포윈도우를 생성합니다
+        //   var infowindow = new window.kakao.maps.InfoWindow({
+        //     content: iwContent,
+        //     removable: iwRemoveable,
+        //   });
+
+        //   // 인포윈도우를 마커위에 표시합니다
+        //   infowindow.open(map, marker);
+
+        //   // 지도 중심좌표를 접속위치로 변경합니다
+        //   map.setCenter(locPosition);
+        // }
       });
     };
 
@@ -65,4 +146,4 @@ function TripDetailMap() {
   );
 }
 
-export default TripDetailMap;
+export default TripEditMap;
