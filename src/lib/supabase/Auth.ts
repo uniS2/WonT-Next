81e36fc45ebe98efdb4cfa48108112b5b45f41b6
Jeon SunYong <sunyong0725@naver.com>
2024-02-19 23:28:50 +0900

    Refactor : Auth 분리

---
+++ src/lib/supabase/Auth.ts
@@ -0,0 +1,73 @@
+import supabase from "./supabase";
+
+// 구글 로그인 기능
+export const signInWithGoogle = async () => {
+  try {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        queryParams: {
+          redirectTo: "http://localhost:3000/main",
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// 카카오 로그인 기능
+export const signInWithKakao = async () => {
+  try {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "kakao",
+      options: {
+        redirectTo: "http://localhost:3000/main",
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// 깃허브 로그인
+export const signInWithGitHub = async () => {
+  try {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:3000/main",
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
