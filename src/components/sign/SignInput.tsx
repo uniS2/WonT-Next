0ab58584c0198d5ec02f1d2d90aec8e109c9c6c0
Jeon SunYong <sunyong0725@naver.com>
2024-02-20 15:37:48 +0900

    Feat : 회원가입을 위한 프로퍼티 추가

--- src/components/sign/SignInput.tsx
+++ src/components/sign/SignInput.tsx
@@ -4,9 +4,17 @@ interface SignInputProps {
   text: string;
   placeholder: string;
   type: "email" | "password" | "text";
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SignInput: React.FC<SignInputProps> = ({ text, placeholder, type }) => {
+const SignInput: React.FC<SignInputProps> = ({
+  text,
+  placeholder,
+  type,
+  value,
+  onChange,
+}) => {
   return (
     <div className="pt-[1.5rem] min-w-[18.75rem]  flex flex-col gap-3 border-b-2 border-primary mx-auto pb-[0.313rem]">
       <p className="text-[1rem]">{text}</p>
@@ -14,6 +22,9 @@ const SignInput: React.FC<SignInputProps> = ({ text, placeholder, type }) => {
         type={type}
         placeholder={placeholder}
         className="text-[1rem]"
+        value={value}
+        onChange={onChange}
+        required
       />
     </div>
   );
