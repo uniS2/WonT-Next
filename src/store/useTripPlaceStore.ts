eb6833d880df33d356187e943e601e93cbc5e598
hyeonjuuu <janghyeonjuu@gmail.com>
2024-02-29 23:44:10 +0900

    [Feat] TripEdit 여행 일정 드래그앤드롭 기능 구현 진행중

--- src/store/useTripPlaceStore.ts
+++ src/store/useTripPlaceStore.ts
@@ -1,25 +1,25 @@
 import { create } from "zustand";
 
-interface TripPlaceState {
-  place: string[] | [];
-  setPlace: (data: string[] | []) => void;
-}
-
-export const useTripPlaceStore = create<TripPlaceState>()((set) => ({
-  place: [],
-  setPlace: (data: string[] | []) => set({ place: data }),
-}));
-/* -------------------------------------------------------------------------- */
-
 // interface TripPlaceState {
-//   place: { [key: string]: string[] }[];
-//   setPlace: (data: { [key: string]: string[] }[]) => void;
+//   place: string[] | [];
+//   setPlace: (data: string[] | []) => void;
 // }
 
-// export const useTripPlaceStore = create<TripPlaceState>((set) => ({
+// export const useTripPlaceStore = create<TripPlaceState>()((set) => ({
 //   place: [],
-//   setPlace: (data: { [key: string]: string[] }[]) => set({ place: data }),
+//   setPlace: (data: string[] | []) => set({ place: data }),
 // }));
+/* -------------------------------------------------------------------------- */
+
+interface TripPlaceState {
+  place: { [key: string]: string[] }[];
+  setPlace: (data: { [key: string]: string[] }[]) => void;
+}
+
+export const useTripPlaceStore = create<TripPlaceState>((set) => ({
+  place: [],
+  setPlace: (data: { [key: string]: string[] }[]) => set({ place: data }),
+}));
 
 interface DayState {
   day: number;
