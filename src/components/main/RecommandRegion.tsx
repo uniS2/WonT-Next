7fc4b3b3b3d752fadbbe4e00699e79efd1b78085
Jeon SunYong <sunyong0725@naver.com>
2024-03-04 18:16:24 +0900

    Feat : map 함수 돌릴 이미지 추가

--- src/components/main/RecommandRegion.tsx
+++ src/components/main/RecommandRegion.tsx
@@ -1,51 +1,60 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import MyBookmarkIcon from "../mypage/MyBookmarkIcon";
+import { TOUR_BASE_AREA } from "@/lib/tour/tour";
+import Image from "next/image";
+
+type RegionDataType = {
+  rnum: number;
+  code: string;
+  name: string;
+  url: string;
+};
 
 const RecommandRegion = () => {
-  const test = () => {
-    console.log("test");
-  };
+  const [regionData, setRegionData] = useState<RegionDataType[] | null>(null);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(TOUR_BASE_AREA);
+      const json = await response.json();
+
+      const dataWithImages = json.response.body.items.item.map(
+        (item: RegionDataType, key: number) => ({
+          ...item,
+          url: `/images/local${key}.jpg`,
+        }),
+      );
+      setRegionData(dataWithImages);
+    })();
+  }, []);
 
   return (
-    <div className=" w-[320px]">
+    <div className=" w-[320px] h-[600px] overflow-auto">
       <div className="flex justify-between border-b-2 items-center my-[26px]">
-        {/* Tour API 사용 후 map 사용 예정 */}
-        <p className="text-[24px] font-bold">국내 추천 여행지</p>
+        <p className="text-[24px] font-bold">국내 여행지</p>
         <Link href={"/search"}>
           <IoSearch size={20} />
         </Link>
       </div>
-      <div className="grid grid-cols-2 gap-6 ">
-        <div className="rounded-md h-[148px] w-[148px] bg-primary relative hover:scale-110 transition">
-          <Link href={"/search"}>
-            <div>이미지</div>
-          </Link>
-          <button
-            type="button"
-            className="absolute right-3 top-3"
-            onClick={test} // 북마크 후 마이페이지 나의 계정에 들어가게끔 구현
-          >
-            <MyBookmarkIcon size="26px" fill="#363636" />
-          </button>
-        </div>
-        <div className="rounded-md h-[148px] w-[148px] bg-primary relative">
-          <button type="button" className="absolute right-3 top-3">
-            <MyBookmarkIcon size="26px" fill="#363636" />
-          </button>
-        </div>
-        <div className="rounded-md h-[148px] w-[148px] bg-primary relative">
-          <button type="button" className="absolute right-3 top-3">
-            <MyBookmarkIcon size="26px" fill="#363636" />
-          </button>
-        </div>
-        <div className="rounded-md h-[148px] w-[148px] bg-primary relative">
-          <button type="button" className="absolute right-3 top-3">
-            <MyBookmarkIcon size="26px" fill="#363636" />
-          </button>
-        </div>
-      </div>
+      <ul className="grid grid-cols-2 gap-6">
+        {regionData?.map((region: RegionDataType) => {
+          return (
+            <div
+              key={region.rnum}
+              className="relative rounded-md h-[138px] w-[138px] overflow-hidden"
+              style={{
+                backgroundImage: `url(${region.url})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="absolute inset-0 bg-primary bg-opacity-50 flex items-center justify-center text-white text-xl rounded-md opacity-0 hover:opacity-100 transition-opacity">
+                {region.name}
+              </div>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 };
