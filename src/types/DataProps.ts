fe5b1452d421091edadf482993e35a7c76eedf64
Jeon SunYong <sunyong0725@naver.com>
2024-03-05 18:00:03 +0900

    Feat : type 분리

---
+++ src/types/DataProps.ts
@@ -0,0 +1,15 @@
+export default interface DataProps {
+  id: number;
+  name: string;
+  engName: string;
+  info: string;
+  faP: string;
+  faP2: string;
+  faP3: string;
+  faH: string;
+  faH2: string;
+  faH3: string;
+  faR: string;
+  faR2: string;
+  faR3: string;
+}
