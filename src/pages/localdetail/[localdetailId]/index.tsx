7786bf3afb957c57c259e959824fc6531d0b8acc
Jeon SunYong <sunyong0725@naver.com>
2024-03-06 17:24:55 +0900

    Style : 스타일링 변경

--- src/pages/localdetail/[localdetailId]/index.tsx
+++ src/pages/localdetail/[localdetailId]/index.tsx
@@ -35,7 +35,7 @@ const LocalDetail = () => {
   return (
     <LocalDetailLayout>
       {localData && (
-        <div className="w-[500px] h-auto bg-primary bg-opacity-10 rounded-lg p-[20px]">
+        <div className="mt-[30px] w-[360px] h-auto bg-primary bg-opacity-10 rounded-lg p-[20px]">
           <div className="flex flex-col gap-1">
             <p className="text-[20px] font-suitBold">{localData.name}</p>
             <p className="text-slate-500">{localData.engName}</p>
@@ -73,7 +73,7 @@ const LocalDetail = () => {
           <Link href={"/search"}>
             <button
               type="button"
-              className="w-[460px] h-[50px] bg-primary flex flex-col items-center justify-center mt-[15px] hover:bg-point"
+              className="w-[320px] h-[50px] bg-primary flex flex-col items-center justify-center mt-[15px] hover:bg-point"
             >
               <p>인기 장소 찾아보기</p>
             </button>
