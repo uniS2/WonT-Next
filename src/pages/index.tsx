fe047478d4b6f6ed1a78a80bf40531da8f2b010b
Jeon SunYong <sunyong0725@naver.com>
2024-03-06 18:14:44 +0900

    Feat : 애니메이션 설정 추후 이미지 -> 비디오로 변경 예정
    스타일링도 변경 예정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
+import { motion } from "framer-motion";
 import OnBoardingButton from "@/components/onboarding/OnBoardingButton";
 import Layout from "@/layout/onboarding/layout";
 import Image from "next/image";
-function OnBoardingPage() {
+
+const OnBoardingPage: React.FC = () => {
   return (
     <Layout>
       <section className="relative h-screen flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-5">
+        <motion.div
+          className="flex flex-col items-center justify-center gap-5"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1, duration: 1 }}
+        >
           <Image src={"/svg/logo.svg"} alt="로고" width={200} height={100} />
           <div className="text-[16px] text-black mb-[30px]">
             <p>행복한 여행을 위해 일정을 세워보세요</p>
@@ -22,10 +29,10 @@ function OnBoardingPage() {
             href="/signin"
             color="primary"
           />
-        </div>
+        </motion.div>
       </section>
     </Layout>
   );
-}
+};
 
 export default OnBoardingPage;
