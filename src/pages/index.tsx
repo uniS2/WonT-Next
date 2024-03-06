0b7ecb1d0b5d976693d6efbde83d05b17339f186
Jeon SunYong <sunyong0725@naver.com>
2024-03-06 17:26:34 +0900

    Style : 마크업 및 스타일링 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,29 @@
 import OnBoardingButton from "@/components/onboarding/OnBoardingButton";
-import OnBoardingContentBox from "@/components/onboarding/OnBoardingContentBox";
-import OnBoardingSideButton from "@/components/onboarding/OnBoardingSideButton";
 import Layout from "@/layout/onboarding/layout";
+import Image from "next/image";
 function OnBoardingPage() {
   return (
     <Layout>
-      <main>
-        <div className="mt-[120px]">
-          <div className="text-[30px] mb-[10px]">
-            <p>여행</p>
-            <p>계획부터 시작!</p>
+      <section className="relative h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-5">
+          <Image src={"/svg/logo.svg"} alt="로고" width={200} height={100} />
+          <div className="text-[16px] text-black mb-[30px]">
+            <p>행복한 여행을 위해 일정을 세워보세요</p>
           </div>
-          <div className="text-[16px] text-gray-400">
-            <p>행복한 여행을 위해 WonT에서</p>
-            <p>당신의 일정을 세워보세요.</p>
-          </div>
-        </div>
-        <div className="mt-[100px] flex">
-          <OnBoardingSideButton />
-          <OnBoardingContentBox />
-        </div>
-        <div className="flex flex-col gap-5 mt-[80px]">
+          <Image
+            src={"/images/test.jpeg"}
+            alt="로고"
+            width={200}
+            height={100}
+            className="mb-[20px]"
+          />
           <OnBoardingButton
             text="WonT 시작하기"
             href="/signin"
             color="primary"
           />
-          <OnBoardingButton
-            text="회원가입"
-            color="contentSecondary"
-            href="/signup"
-          />
         </div>
-      </main>
+      </section>
     </Layout>
   );
 }
