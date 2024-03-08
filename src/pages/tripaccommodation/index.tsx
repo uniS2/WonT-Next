8100e01029d9524bf0f6cb6ae6b31d44adfc74dd
uniS2 <s2oie1010@gmail.com>
2024-03-08 16:53:29 +0900

    Feat: TripAccommodation 페이지 - 1차 기능 구현 완료
    - LocalItem: Tour API Read 데이터(위치기반 숙소정보)
    - SelectItem: 선택된 숙소 정보
    - 그 외 AccommodationStore 내 selectedAccommodation(선택된 숙소 정보 storage) 이용한 정보 렌더링
    
    Related to #55

--- src/pages/tripaccommodation/index.tsx
+++ src/pages/tripaccommodation/index.tsx
@@ -3,9 +3,20 @@ import HeaderTripSelect from "@/components/header/HeaderTripSelect";
 import TripAccommodationMap from "@/components/tripaccommodation/TripAccommodationMap";
 import TripRegionDaysEdit from "@/components/common/TripRegionDaysEdit";
 import SelectDaysInfo from "@/components/tripselect/SelectDaysInfo";
+import DefaultImage from "@/components/common/DefaultImage";
 import SelectItem from "@/components/tripselect/SelectItem";
+import LocalItem from "@/components/tripselect/LocalItem";
+import ButtonLarge from "@/components/tripselect/ButtonLarge";
+import { AccommodationStore } from "@/store/AccommodationStore";
 
 function TripRegionPage() {
+  const {
+    locationAccommodation,
+    selectedAccommodation,
+    resetSelectedAccommodation,
+  } = AccommodationStore();
+  const isSelected = Boolean(selectedAccommodation);
+
   return (
     <TripAccommodationLayout>
       <HeaderTripSelect isPadding inCloseButton />
@@ -16,16 +27,57 @@ function TripRegionPage() {
           <SelectDaysInfo />
           <button
             type="button"
+            onClick={resetSelectedAccommodation}
             className="w-20 h-7 rounded-md border border-contentMuted  text-sm text-contentMuted hover:bg-secondary hover:border-black hover:text-black hover:font-semibold"
           >
             초기화
           </button>
         </div>
-        <ul className="w-full pl-5 pr-[0.625rem] py-[0.625rem] rounded-[0.625rem] bg-[#E9F0F0]">
-          {/* AccommodationData.map((item) => {firstimage, title, addr1, mapx, mapy}) */}
-          <SelectItem />
+        <ul
+          className={`grid grid-cols-1 lg:grid-cols-2 gap-3 w-full pr-3 py-3 rounded-xl ${isSelected ? "bg-button" : "bg-[#E9F0F0]"}`}
+        >
+          {isSelected ? (
+            selectedAccommodation!.map((accommodation, index) => (
+              <SelectItem
+                key={accommodation.contentid}
+                index={index + 1}
+                title={accommodation.title}
+                addr={`${accommodation.addr2 ? accommodation.addr1 + " " + accommodation.addr2 : accommodation.addr1}`}
+                imgSrc={accommodation.firstimage || accommodation.firstimage2}
+              />
+            ))
+          ) : (
+            <li className="flex gap-5 pl-5 items-center justify-start w-full">
+              <span className="w-5 h-5 rounded-full bg-[#D0CFD7] text-center text-[#F3F5F5] leading-5">
+                1
+              </span>
+              <div className="flex gap-3 items-center w-full p-3 rounded-xl bg-white">
+                <DefaultImage />
+                <p className="text-sm text-contentMuted">
+                  추가된 장소가 없습니다.
+                </p>
+              </div>
+            </li>
+          )}
         </ul>
+        <div
+          className={`pt-5 border-t-4 ${isSelected ? "border-primary" : "border-[#D0CFD7]"}`}
+        >
+          <ul className="grid grid-cols-1 lg:grid-cols-2 gap-3 w-full p-3 rounded-xl bg-[#E9F0F0]">
+            {locationAccommodation &&
+              locationAccommodation.map((location, index) => (
+                <LocalItem
+                  key={index}
+                  id={location.contentid}
+                  title={location.title}
+                  addr={`${location.addr2 ? location.addr1 + " " + location.addr2 : location.addr1}`}
+                  imgSrc={location.firstimage || location.firstimage2}
+                />
+              ))}
+          </ul>
+        </div>
       </section>
+      <ButtonLarge href="/tripeidt" />
     </TripAccommodationLayout>
   );
 }
