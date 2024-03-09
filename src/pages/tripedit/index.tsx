e986cf74a86b81c625348341ad80a4ed365eecc8
uniS2 <s2oie1010@gmail.com>
2024-03-09 23:13:50 +0900

    Feat, Rename: TripDate 페이지 - `DatesStore` storage 변경사항에 맞게 렌더링되도록 컴포넌트 수정
    - TripDays -> TripDate 페이지로 이름 변경
    - storage 변경사항에 맞게 수정
    - TripEdit 페이지 내 `TripRegionDaysEdit` 컴포넌트 적용
    
    Github issue #59

--- src/pages/tripedit/index.tsx
+++ src/pages/tripedit/index.tsx
@@ -1,28 +1,23 @@
+import TripRegionDaysEdit from "@/components/common/TripRegionDaysEdit";
 import SaveButton from "@/components/tripedit/SaveButton";
 import TripDays from "@/components/tripedit/TripDays";
 import TripEditMap from "@/components/tripedit/TripEditMap";
 import TripEditLayout from "@/layout/tripedit/layout";
-import { AccommodationStore } from "@/store/AccommodationStore";
+import { DatesStore } from "@/store/DatesStore";
 
 function TripEdit() {
-  const tripDate = new Date();
+  const { tripDates } = DatesStore();
+  //TODO@uniS2
+  console.log("여행일자", tripDates);
 
   //TODO@uniS2: 각 일자에 맞는 숙박 선택 정보 제공을 위한 storage 초기화
-  const clearAccommodationIdStorage = AccommodationStore.persist.clearStorage;
+  // const clearAccommodationIdStorage = AccommodationsStore.persist.clearStorage;
 
   return (
     <TripEditLayout>
       <div>
         <TripEditMap />
-        <div className="w-full h-[80px] bg-contentSecondary flex justify-between items-center px-5">
-          <div className="flex flex-col">
-            <span className="text-white">제주</span>
-            <span className="text-white">24.01.20 - 24.01.22</span>
-          </div>
-          <button className="border-[1px] w-[56px] h-7 rounded-md border-contentMuted text-contentMuted text-sm">
-            수정
-          </button>
-        </div>
+        <TripRegionDaysEdit />
         <section>
           <div className="mt-7 mb-10">
             <TripDays days="Day 1" date="24.01.20" />
