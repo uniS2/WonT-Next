c9d4f2a78a6c0de8c5ecda5665ad83bd350e2ca1
uniS2 <s2oie1010@gmail.com>
2024-03-11 12:28:00 +0900

    Feat: TripAccommodation_Place 페이지 - 선택한 숙소, 관광 정보 storage 재수정 - `{ [index]: [데이터] }` => `[ [데이터], [데이터], ... ]`

--- src/utils/setTripRange.ts
+++ src/utils/setTripRange.ts
@@ -1,11 +1,3 @@
-import { PlacesObject } from "@/types/DataProps";
-
 export const setTripRange = (range: number) => {
-  const newSelectedPlaces: PlacesObject = {};
-
-  for (let date = 1; date <= range; date++) {
-    newSelectedPlaces[date] = [];
-  }
-
-  return newSelectedPlaces;
+  return new Array(range).fill([]);
 };
