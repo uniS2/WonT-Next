d470e8dc6d1e4bfb53654e32a65b2b10019b3c5a
uniS2 <s2oie1010@gmail.com>
2024-03-11 17:53:35 +0900

    Feat: TripPlace 페이지 - tripDate (useParams 값) 렌더링
    Github issue #58

--- src/components/common/SelectDateInfo.tsx
+++ src/components/common/SelectDateInfo.tsx
@@ -1,15 +1,20 @@
-type SelectDateInfoProps = {
-  totalNumber?: number;
+import { DatesStore } from "@/store/DatesStore";
+
+type SelectDateInfoType = {
+  tripDate?: number;
 };
 
-const SelectDateInfo = ({ totalNumber = 0 }: SelectDateInfoProps) => {
+const SelectDateInfo = ({ tripDate = 0 }: SelectDateInfoType) => {
+  const { tripDates } = DatesStore();
   return (
     <div className="flex gap-[0.625rem] items-end">
-      <span className="text-2xl text-content font-bold">{totalNumber}</span>
+      <span className="text-2xl text-content font-bold">
+        {tripDates?.length || 0}
+      </span>
       <div className="flex gap-[0.125rem] min-w-fit text-sm">
-        <span className="font-medium text-[#2966E3]">n일</span>
+        <span className="font-medium text-[#2966E3]">{tripDate}일</span>
         <span aria-hidden>/</span>
-        <span>n일</span>
+        <span>{tripDates?.length || 0}일</span>
       </div>
     </div>
   );
