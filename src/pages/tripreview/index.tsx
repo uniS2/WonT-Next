1473cb98a5ed3e8106027a7e0ea9b7c4af99798e
hyeonjuuu <janghyeonjuu@gmail.com>
2024-03-17 13:15:27 +0900

    [Feat] 작성 리뷰 렌더링

--- src/pages/tripreview/index.tsx
+++ src/pages/tripreview/index.tsx
@@ -28,8 +28,13 @@ function TripReview() {
 
   return (
     <TripReviewLayout>
-      <div className="flex flex-col mx-16 gap-5 lg:grid lg:grid-cols-4 lg:mx-16 md:grid-cols-3 md:mx-16 md:grid sm:grid sm:grid-cols-2 sm:mx-16">
-        {review?.map((item) => <TripReviewFeed reviewData={item} />)}
+      <div>
+        {/* <button className=" px-6 py-2 bg-primary text-white cursor-pointer w-8 h-5 rounded-md">
+          리뷰 작성
+        </button> */}
+        <div className="flex flex-col mx-16 gap-5 lg:grid lg:grid-cols-4 lg:mx-16 md:grid-cols-3 md:mx-16 md:grid sm:grid sm:grid-cols-2 sm:mx-16">
+          {review?.map((item) => <TripReviewFeed reviewData={item} />)}
+        </div>
       </div>
     </TripReviewLayout>
   );
