6575e7bfc34dc9cbdb446fc27d9def2b257f0a1e
hyeonjuuu <janghyeonjuu@gmail.com>
2024-03-17 14:56:55 +0900

    [Feat] TripEdit Header 설정

--- src/components/header/HeaderTripEdit.tsx
+++ src/components/header/HeaderTripEdit.tsx
@@ -1,19 +1,21 @@
 import { AiOutlineHome } from "react-icons/ai";
 import { PiUser } from "react-icons/pi";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
+import { useRouter } from "next/router";
 
 function HeaderTripEdit() {
+  const router = useRouter();
   return (
     <header className="bg-white-opacity-50 p-5">
       <nav className="flex justify-between gap-3 bg-white-opacity-50">
-        <button>
+        <button onClick={() => router.back()}>
           <IoArrowBackCircleOutline size="30px" color="#363636" />
         </button>
         <div className="flex gap-3">
-          <button>
+          <button onClick={() => router.push("/main")}>
             <AiOutlineHome size="30px" color="#363636" />
           </button>
-          <button>
+          <button onClick={() => router.push("/mypage")}>
             <PiUser size="30px" color="#363636" />
           </button>
         </div>
